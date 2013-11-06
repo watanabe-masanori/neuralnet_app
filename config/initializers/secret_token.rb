da2140d5f4b3200a4fba894076823ee0f848482f
watanabe <alcange@hotmail.co.jp>
2013-11-06 18:56:19 +0900

    ニューラルネットワークサンプルのRailsの初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neuralnet::Application.config.secret_key_base = 'e696200a2c8a36b94ce947c788eef8e1551ee554eff2b699ce0240486a3c192e9b00db380e5781cc12d839b344b094761e6f7b07e44bf30cf2e1015bd8d04def'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Neuralnet::Application.config.session_store :cookie_store, key: '_neuralnet_session'
